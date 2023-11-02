20726cc90cfa52a8036d58a4a8f4014828021ee4
“Alison <alison.obright@gmail.com>
2023-11-02 15:29:35 -0500

    new attempt

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Analyzing",p:["p1"]},{w:"data",p:["p1","p4"]},{w:"from",p:["p1"]},{w:"paranormal",p:["p1","p4","p5","p6","p7","p8"]},{w:"investigation",p:["p1","p2","p4","p5","p6","p7","p8"]},{w:"Choosing",p:["p2"]},{w:"site",p:["p2"]},{w:"Conducting",p:["p3"]},{w:"exploratory",p:["p3"]},{w:"interviews",p:["p3"]},{w:"Gathering",p:["p4"]},{w:"during",p:["p4"]},{w:"Method",p:["p5"]},{w:"of",p:["p5","p6","p7"]},{w:"Table",p:["p6"]},{w:"common",p:["p6"]},{w:"technologies",p:["p6"]},{w:"and",p:["p6"]},{w:"prices",p:["p6"]},{w:"History",p:["p7"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8, 7, 9];
+ph["p3"]=[10, 11, 12];
+ph["p4"]=[13, 4, 14, 6, 7];
+ph["p5"]=[15, 16, 6, 7];
+ph["p6"]=[17, 16, 18, 6, 7, 19, 20, 21];
+ph["p7"]=[22, 16, 6, 7];
+ph["p8"]=[6, 7];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {3:5,4:5,5:-1,7:-1,8:-1,1:5,6:4,2:5};
